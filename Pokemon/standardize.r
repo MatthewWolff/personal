0fd4fc27756edadd562a1f3467006c7cf28570cf
Matthew Wolff <mm.wolff@chi-squared.org>
2018-02-19 17:51:52 -0600

    found bug with NAs entering numerify_categorical
    Former-commit-id: 872e3123909fae1a0ab5dfd3aa94d629d9cb49f8

--- Pokemon/standardize.r
+++ Pokemon/standardize.r
@@ -7,6 +7,8 @@ numerify_categorical <- function(categorical){
   uniq <- unique(categorical)
   categorical <- factor(categorical, levels = uniq, ordered = TRUE) # ordered for deterministic behavior
   sorter <- lapply(uniq, function(x) {
+    if(is.na(x))
+      return(NULL)
     rtn <- integer(length(uniq)); 
     rtn[x] <- 1; 
     if(as.numeric(x) == length(rtn))
@@ -18,7 +20,7 @@ numerify_categorical <- function(categorical){
   return(sorter[categorical])
 }
 binarize_categorical <- function(binary_data){
-  return(sapply(as.logical(binary_data), function(x) if(x) return(1) else return(-1)))
+  return(sapply(as.logical(binary_data), function(x) if(x) 1 else -1))
 }
 
 # normalizing data
@@ -39,7 +41,6 @@ pd <- pokemon %>%
          Power = as.numeric(scale(Power))) %>%
   mutate(Type1 = numerify_categorical(Type1), # take care of categorical variables
          Type2 = numerify_categorical(Type2),
-         Legendary = binarize_categorical(Legendary)) # convert binary to numeric
-
-test1 <- numerify_categorical(pokemon$Type.1); test1 # correct
-test2 <- numerify_categorical(pokemon$Generation); test2 # correct
+         Gen = numerify_categorical(Gen),
+         Legendary = binarize_categorical(Legendary)) %>% # convert binary to numeric
+  mutate(Type2 = lapply(pd$Type2, function(x) if(!is.null(x)) x else NA)) # turn NULLs into NA
