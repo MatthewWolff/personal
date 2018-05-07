0983bf4025dd6ef4ef3c4bb9d5307b2a0cbbab9a
Matthew Wolff <mm.wolff@chi-squared.org>
2018-05-06 19:24:43 -0500

    updated
    Former-commit-id: 704467245135bcc9c42e6c02f21dde556b7bb9f5

---
+++ no_sudo_zsh_install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+mkdir -p $HOME/libraries
+# retrieve zsh files
+wget -O zsh.tar.gz https://sourceforge.net/projects/zsh/files/latest/download --no-check-certificate
+# unpack and compile
+mkdir $HOME/libraries/zsh && tar -xvzf zsh.tar.gz -C $HOME/libraries/zsh --strip-components 1 && rm zsh.tar.gz
+cd $HOME/libraries/zsh
+./configure --prefix=$HOME/libraries
+make
+make install
+
+# automatically swap into zsh? can also throw this in the .bashrc
+# echo "exec $HOME/libraries/bin/zsh -l" >> ~/.profile
+echo "exec $HOME/libraries/bin/zsh -l" >> ~/.bashrc
+
+# Install oh-my-zsh and grab custom .zshrc and funky theme files from github
+git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
+wget https://raw.githubusercontent.com/MatthewWolff/Scraps/master/.zshrc -O $HOME/.zshrc
+wget https://raw.githubusercontent.com/MatthewWolff/Scraps/master/funky.zsh-theme -O $HOME/.oh-my-zsh/themes/funky.zsh-theme
+# install syntax highlighting
+cd $HOME/.oh-my-zsh && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+  # echo "source ~/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
+
+# source to demo changes
+source $HOME/.zshrc
+exec $HOME/libraries/bin/zsh -l
