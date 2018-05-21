6bbc98242ccb60349a425f5a70dbe945b291ec8e
Matthew Wolff <matthew.wolff@wisc.edu>
2018-05-21 17:30:10 -0500

    using curl instead of wget for compatibility
    Former-commit-id: 2035dae5ce2c332f55d7736791597e95dcc4b362

--- no_sudo_zsh_install.sh
+++ no_sudo_zsh_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mkdir -p $HOME/libraries
 # retrieve zsh files
-wget -O zsh.tar.gz https://sourceforge.net/projects/zsh/files/latest/download --no-check-certificate
+curl -o zsh.tar.gz https://sourceforge.net/projects/zsh/files/latest/download --no-check-certificate
 # unpack and compile
 mkdir $HOME/libraries/zsh && tar -xvzf zsh.tar.gz -C $HOME/libraries/zsh --strip-components 1 && rm zsh.tar.gz
 cd $HOME/libraries/zsh
@@ -16,12 +16,12 @@ echo "exec $HOME/libraries/bin/zsh -l" >> ~/.bashrc
 # Install oh-my-zsh and grab custom .zshrc and funky theme files from github
 git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
-wget https://raw.githubusercontent.com/MatthewWolff/Scraps/master/.zshrc -O $HOME/.zshrc
-wget https://raw.githubusercontent.com/MatthewWolff/Scraps/master/funky.zsh-theme -O $HOME/.oh-my-zsh/themes/funky.zsh-theme
+curl -o $HOME/.zshrc https://raw.githubusercontent.com/MatthewWolff/Scraps/master/.zshrc
+curl -o $HOME/.oh-my-zsh/themes/funky.zsh-theme https://raw.githubusercontent.com/MatthewWolff/Scraps/master/funky.zsh-theme 
 # install syntax highlighting
-cd $HOME/.oh-my-zsh && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
-  # echo "source ~/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
+cd $HOME/.oh-my-zsh && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ~/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
 
-# source to demo changes
+# change into zsh
 source $HOME/.zshrc
 exec $HOME/libraries/bin/zsh -l
