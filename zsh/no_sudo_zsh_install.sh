edc3d525f01fe0bd06b2d0aaee148e39400e1c91
Matthew Wolff <mwolff3@wisc.edu>
2018-05-25 13:42:32 -0500

    funky -> wolffy
    Former-commit-id: 3a01220a3f207aee28d4a79f5c7a3537fb5c860d

--- zsh/no_sudo_zsh_install.sh
+++ zsh/no_sudo_zsh_install.sh
@@ -16,7 +16,7 @@ echo "exec $HOME/libraries/bin/zsh -l" >> ~/.bashrc
 # Install oh-my-zsh and grab custom .zshrc and funky theme files from github
 git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 curl -o $HOME/.zshrc https://raw.githubusercontent.com/MatthewWolff/Scraps/master/zsh/.zshrc
-curl -o $HOME/.oh-my-zsh/themes/funky.zsh-theme https://raw.githubusercontent.com/MatthewWolff/Scraps/master/zsh/funky.zsh-theme 
+curl -o $HOME/.oh-my-zsh/themes/wolffy.zsh-theme https://raw.githubusercontent.com/MatthewWolff/Scraps/master/zsh/wolffy.zsh-theme 
 # install syntax highlighting
 cd $HOME/.oh-my-zsh && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 # echo "source ~/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc  # already in there
