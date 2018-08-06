383a051f1a4bce2979da3d0b978feaf6fd3570db
Matthew Wolff <matthew.wolff@wisc.edu>
2018-08-06 01:50:16 -0500

    updated with acquired bash knowledge
    Former-commit-id: d26221baebb5b141e107af7bd7814458a970af7f

--- zsh/no_sudo_zsh_install.sh
+++ zsh/no_sudo_zsh_install.sh
@@ -9,7 +9,7 @@ echo "Downloaded zsh source..."
 # unpack and compile
 tar -xzf zsh.tar.gz -C $INSTALL_LOC/zsh --strip-components 1 && rm zsh.tar.gz
 echo "Configuring zsh..."
-$INSTALL_LOC/zsh/configure --prefix=$INSTALL_LOC > /dev/null && echo "  -Success"
+cd $INSTALL_LOC/zsh && ./configure --prefix=$INSTALL_LOC > /dev/null && echo "  -Success"
 echo "Compiling zsh binaries..."
 make &> /dev/null || { echo "ERR: You may not have sufficient rights to build zsh here." 1>&2; exit 1; }
 make install &> /dev/null && echo "  -Success"
@@ -30,10 +30,10 @@ curl -o $HOME/.oh-my-zsh/themes/wolffy.zsh-theme https://raw.githubusercontent.c
 echo "Refreshed wolffy.zsh-theme"
 # install syntax highlighting
 [[ -d $HOME/.oh-my-zsh/zsh-syntax-highlighting ]] || \
-  cd $HOME/.oh-my-zsh && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git &>/dev/null && cd - >/dev/null
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/zsh-syntax-highlighting &>/dev/null
 grep -q "zsh-syntax-highlighting" ~/.zshrc || \
   echo "source ~/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
   
 # change into zsh
 echo $'Installation of zsh and oh-my-zsh complete!\n'
-exec $INSTALL_LOC/bin/zsh -l
+cd && exec $INSTALL_LOC/bin/zsh -l
