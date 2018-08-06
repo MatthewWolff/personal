8d25db278a5fe8fcff22bf883caae57985a98110
Matthew Wolff <matthew.wolff@wisc.edu>
2018-08-06 01:39:45 -0500

    updated with acquired bash knowledge
    Former-commit-id: 9a7441b45559f60bc8c6fecdade8b5358d566f0c

--- zsh/no_sudo_zsh_install.sh
+++ zsh/no_sudo_zsh_install.sh
@@ -1,26 +1,39 @@
-#!/usr/bin/env bash
-mkdir -p $HOME/libraries
-# retrieve zsh files
-curl -o zsh.tar.gz https://phoenixnap.dl.sourceforge.net/project/zsh/zsh/5.5.1/zsh-5.5.1.tar.gz # doesn't follow redirect
-# unpack and compile
-mkdir $HOME/libraries/zsh && tar -xvzf zsh.tar.gz -C $HOME/libraries/zsh --strip-components 1 && rm zsh.tar.gz
-cd $HOME/libraries/zsh
-./configure --prefix=$HOME/libraries
-make
-make install
+#!/bin/bash
+set -e
 
-# automatically swap into zsh? can also throw this in the .bashrc
-# echo "exec $HOME/libraries/bin/zsh -l" >> ~/.profile
-echo "exec $HOME/libraries/bin/zsh -l" >> ~/.bashrc
+INSTALL_LOC=$HOME/.zsh_installation
+mkdir -p $INSTALL_LOC/zsh
+# retrieve zsh files
+curl -o zsh.tar.gz https://phoenixnap.dl.sourceforge.net/project/zsh/zsh/5.5.1/zsh-5.5.1.tar.gz &> /dev/null
+echo "Downloaded zsh source..."
+# unpack and compile
+tar -xzf zsh.tar.gz -C $INSTALL_LOC/zsh --strip-components 1 && rm zsh.tar.gz
+echo "Configuring zsh..."
+$INSTALL_LOC/zsh/configure --prefix=$INSTALL_LOC > /dev/null && echo "  -Success"
+echo "Compiling zsh binaries..."
+make &> /dev/null || { echo "ERR: You may not have sufficient rights to build zsh here." 1>&2; exit 1; }
+make install &> /dev/null && echo "  -Success"
+
+
+# automatically swap into zsh
+source ~/.bashrc >> ~/.bash_profile
+[[ -d $INSTALL_LOC/bin ]] && echo "exec $INSTALL_LOC/bin/zsh -l" >> ~/.bashrc
+echo "Modified ~/.bashrc to switch into zsh..."
 
 # Install oh-my-zsh and grab custom .zshrc and wolffy theme files from github
-git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
-curl -o $HOME/.zshrc https://raw.githubusercontent.com/MatthewWolff/Scraps/master/zsh/.zshrc
-curl -o $HOME/.oh-my-zsh/themes/wolffy.zsh-theme https://raw.githubusercontent.com/MatthewWolff/Scraps/master/zsh/wolffy.zsh-theme 
+if [[ ! -d ~/.oh-my-zsh ]]; then
+  git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh  &>/dev/null
+  curl -o $HOME/.zshrc https://raw.githubusercontent.com/MatthewWolff/Scraps/master/zsh/.zshrc &>/dev/null
+  echo "Installed oh-my-zsh..."
+fi
+curl -o $HOME/.oh-my-zsh/themes/wolffy.zsh-theme https://raw.githubusercontent.com/MatthewWolff/Scraps/master/zsh/wolffy.zsh-theme &>/dev/null
+echo "Refreshed wolffy.zsh-theme"
 # install syntax highlighting
-cd $HOME/.oh-my-zsh && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-# echo "source ~/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc  # already in there
-
+[[ -d $HOME/.oh-my-zsh/zsh-syntax-highlighting ]] || \
+  cd $HOME/.oh-my-zsh && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git &>/dev/null && cd - >/dev/null
+grep -q "zsh-syntax-highlighting" ~/.zshrc || \
+  echo "source ~/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
+  
 # change into zsh
-source $HOME/.zshrc
-exec $HOME/libraries/bin/zsh -l
+echo $'Installation of zsh and oh-my-zsh complete!\n'
+exec $INSTALL_LOC/bin/zsh -l
