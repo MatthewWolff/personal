6108e6e2101669949392b4d4e1c4f592290f7a63
Matthew Wolff <mwolff3@wisc.edu>
2018-08-06 01:50:16 -0500

    updated with acquired bash knowledge
    Former-commit-id: d26221baebb5b141e107af7bd7814458a970af7f

--- zsh/sudo_install_zsh.sh
+++ zsh/sudo_install_zsh.sh
@@ -6,24 +6,37 @@ system() {
       Darwin*)    machine=Mac;;
       CYGWIN*)    machine=Cygwin;;
       MINGW*)     machine=MinGw;;
-      *)          machine="UNKNOWN:${unameOut}"
+      *)          machine="UNKNOWN:${unameOut}";;
   esac
-echo ${machine}
+  echo $machine
 }
 
 if ! which zsh > /dev/null; then # need to install zsh
-  echo "installing zsh if on mac or linux"
+  echo "Installing zsh if on Mac or Linux"
   case "$(system)" in
-	Mac*)	which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && sudo brew install zsh;;
-	Linux*)	sudo apt-get install zsh;;
+    Mac*)    which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && sudo brew install zsh;;
+    Linux*)  sudo apt-get install zsh;;
+    *)       echo "RIP"; exit 1;;
   esac
 fi
 
-# install oh-my-zsh and grab custom .zshrc and wolffy theme files from github
-git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
-curl -o $HOME/.zshrc https://raw.githubusercontent.com/MatthewWolff/Scraps/master/zsh/.zshrc 
-curl -o $HOME/.oh-my-zsh/themes/wolffy.zsh-theme https://raw.githubusercontent.com/MatthewWolff/Scraps/master/zsh/wolffy.zsh-theme 
+# install oh-my-zsh and grab custom .zshrc from github
+if [[ ! -d $HOME/.oh-my-zsh ]]; then
+  git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh  &>/dev/null
+  curl -o $HOME/.zshrc https://raw.githubusercontent.com/MatthewWolff/Scraps/master/zsh/.zshrc &>/dev/null
+  echo "Installed oh-my-zsh..."
+fi
+
+# wolffy theme
+curl -o $HOME/.oh-my-zsh/themes/wolffy.zsh-theme https://raw.githubusercontent.com/MatthewWolff/Scraps/master/zsh/wolffy.zsh-theme &>/dev/null
+echo "Refreshed wolffy.zsh-theme"
 
 # install syntax highlighting
-cd $HOME/.oh-my-zsh && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+[[ -d $HOME/.oh-my-zsh/zsh-syntax-highlighting ]] || \
+   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/zsh-syntax-highlighting &>/dev/null
+grep -q "zsh-syntax-highlighting" ~/.zshrc || \
+  echo "source ~/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
+  
+# change into zsh
+echo $'Installation of zsh and oh-my-zsh complete!\n'
 exec zsh -l
