352aa5dca3de8023f3b6f7c36585e18606022f5d
Matthew Wolff <mwolff3@wisc.edu>
2018-12-28 18:21:22 -0600

    corrected anaconda install

--- new_computer.sh
+++ new_computer.sh
@@ -1,10 +1,12 @@
 #/bin/bash
+set -x
 xcode-select --install
 /usr/bin/ruby -e `curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install`
 chsh -s /bin/zsh
 brew tap caskroom/cask # add another formula repository
-brew cask install anaconda2 anaconda3 java brew mactex & 
-brew install wget zsh r scala apach-spark hadoop &
+brew cask install anaconda java brew mactex & 
+brew install python3 wget zsh r scala apach-spark hadoop &
+# echo 'export PATH=/usr/local/anaconda3/bin:$PATH' >> ~/.zsh
 Rscript -e 'install.packages("tidyverse")' &
 open https://github.com/fikovnik/ShiftIt/releases/download/version-1.6.6/ShiftIt-1.6.6.zip \
      https://iterm2.com/downloads/stable/latest \ 
