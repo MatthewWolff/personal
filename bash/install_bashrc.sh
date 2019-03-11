2d275472028746373974feec5885cc5e77d59712
Matthew Wolff <mm.wolff@chi-squared.org>
2019-03-10 20:00:02 -0500

    added source command at end of .bashrc install script

--- bash/install_bashrc.sh
+++ bash/install_bashrc.sh
@@ -10,12 +10,9 @@ system() {
   esac
   echo $machine
 }
-WHITE="\033[1m\033[37m"
-RESET="\033[0m"
-stdout() { echo -e $WHITE$*$RESET; }
-
-cp ~/.bashrc ~/.backup_bashrc
-curl -so ~/.bashrc https://raw.githubusercontent.com/MatthewWolff/Personal/master/bash/.bashrc
-[[ $(system) = Linux ]] && perl -pi -e 's/ls -G/ls --color/' $HOME/.bashrc
-[[ $USER = root ]] && perl -pi -e 's/\$WHITE(?=\\u\$YELLOW@)/\$RED/' $HOME/.bashrc # root coloring
-stdout "set up ~/.bashrc"
+rc=$HOME/.bashrc
+cp $rc $HOME/.backup_bashrc
+curl -so $rc https://raw.githubusercontent.com/MatthewWolff/Personal/master/bash/.bashrc
+[[ $(system) = Linux ]] && perl -pi -e 's/ls -G/ls --color/' $rc # proper ls command
+[[ $USER = root ]] && perl -pi -e 's/\$WHITE(?=\\u\$YELLOW@)/\$RED/' $rc # root coloring
+. $rc
