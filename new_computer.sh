bdc92480af217390a2db6a0e8dbfb987f0290974
Matthew Wolff <matthew.wolff@wisc.edu>
2024-07-27 15:31:38 -0700

    Update for new work macbook install

--- new_computer.sh
+++ new_computer.sh
@@ -3,28 +3,25 @@ set -x
 
 # primary utilities (xcode + brew)
 xcode-select --install
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # brew installs
 brew update
 brew tap homebrew/cask 
 brew install r # install R before Rstudio
-brew install --cask anaconda iterm2 visual-studio-code gimp rstudio pycharm intellij-idea \
-  spectacle google-chrome spotify sublime-text vlc # first line: dev tools \\ second line: utility
-brew install openjdk@8 hub wget zsh bash scala bfg node \
-  http-server shellcheck shpotify gdrive lolcat tree
+brew install --cask docker anaconda qlmarkdown visual-studio-code sublime-text rstudio pycharm intellij-idea \
+  rectangle google-chrome spotify raycast vlc gimp iterm2 notion anki git-credential-manager-core # first line: dev tools \\ second line: utility
+brew install awscli imagemagick hub zsh bash nvm http-server shellcheck shpotify gdrive lolcat tree grep curl wget sqlite ssh-copy-id
 brew tap microsoft/git
+brew tap amazon/amazon "ssh://git.amazon.com/pkg/HomebrewAmazon"
 brew install --cask git-credential-manager-core
 brew cleanup
 
+# install latest version of node
+nvm install node
+
 # r packages
 Rscript -e 'install.packages("tidyverse", repos = "http://cran.us.r-project.org")' &
-
-# chrome extensions
-open https://chrome.google.com/webstore/detail/dark-reader/eimadpbcbfnmbkopoojfekhnkhdbieeh \
-  https://chrome.google.com/webstore/detail/adblock/gighmmpiobklfepjocnamgkkbiglidom \
-  https://chrome.google.com/webstore/detail/backspace-to-go-back/nlffgllnjjkheddehpolbanogdeaogbc \
-  -a "/Applications/Google Chrome.app/"
 
 # customization + personal credentials
 mkdir -p $HOME/scripts $HOME/development
